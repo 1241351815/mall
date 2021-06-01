94afaed68d3d9bad6a05972a764923f8c14a8eb7
1241351815 <1241351815@qq.com>
2021-06-01 09:48:10 +0800

    路由调整

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,77 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '../pages/home'
+import Index from '../pages/index'
+import Product from '../pages/product'
+import Detail from '../pages/detail'
+import Order from '../pages/order'
+import OrderList from '../pages/orderList'
+import OrderConfirm from '../pages/orderConfirm'
+import OrderPay from '../pages/orderPay'
+import OrderAlipay from '../pages/orderAlipay'
+import Cart from '../pages/cart'
+
+Vue.use(Router)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'home',
+    component: Home,
+    redirect: '/index',
+    children: [
+      {
+        path: '/',
+        name: 'index',
+        componten: Index
+      },
+      {
+        path: '/product/:id',
+        name: 'product',
+        componten: Product
+      },
+      {
+        path: '/detail/:id',
+        name: 'detail',
+        componten: Detail
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/order',
+    name: 'order',
+    component: Order,
+    children: [
+      {
+        path: '/list',
+        name: 'order-list',
+        componten: OrderList
+      },
+      {
+        path: '/confirm',
+        name: 'order-confirm',
+        componten: OrderConfirm
+      },
+      {
+        path: '/pay',
+        name: 'order-pay',
+        componten: OrderPay
+      },
+      {
+        path: '/Alipay',
+        name: 'order-Alipay',
+        componten: OrderAlipay
+      }
+    ]
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new Router({
   routes
 })
 
